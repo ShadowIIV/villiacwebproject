53dade702aacde0e19e072977d18f977795434e0
ッ <104748940+ShadowIIV@users.noreply.github.com>
2022-05-10 03:12:45 -0500

    Changed some files and added

---
+++ Script/script.js
@@ -0,0 +1,19 @@
+function bgm() {
+  document.getElementById('bgm()').volume = 0.2;
+  const audio = document.getElementById('bgm()');
+  const playlist = ['Audio/jazzy-abstract-beat.mp3', 'Audio/Jazzy_Music1.mp3'];
+  let current = 0;
+
+  // Loads next song
+
+  audio.onended = () => {
+    current++;
+    if (current >= playlist.length) { current = 0; }
+    audio.src = playlist[current];
+    audio.pause();
+    audio.load();
+    audio.play();
+  };
+}
+
+
